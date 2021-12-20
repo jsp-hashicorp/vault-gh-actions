3a0488deaa1f051bf82d49618746f4e6c40872bc
jsp-hashicorp <jsp@hashicorp.com>
2021-12-20 22:13:46 +0900

    change width & height

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -2,6 +2,7 @@
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
 
+
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Meow!</title></head>
